622bdc25f2557316804a1f873bc8185f8a75598a
Jean-LouisH <LouisLou512.e@gmail.com>
2020-02-12 11:41:20 -0500

    Added MIT license

--- Lilliputian/main.cpp
+++ Lilliputian/main.cpp
@@ -1,5 +1,11 @@
-#include <stdio.h>
-
+/**
+*                          Lilliputian
+*
+* main
+*
+* Copyright (c) 2020 Jean-Louis Haywood. All rights reserved.
+* License: https://github.com/Jean-LouisH/Lilliputian/blob/master/LICENSE
+*/
 
 int main(int argc, char* argv[])
 {
