b03e8a0e179f7c4959070ebf2df4dd7f026f38f1
Jean-LouisH <LouisLou512.e@gmail.com>
2020-02-15 09:54:17 -0500

    Added Aliases and Constants

---
+++ Lilliputian/Utilities/Definitions/Aliases.hpp
@@ -0,0 +1,8 @@
+#pragma once
+
+#include <stdint.h>
+
+namespace Lilliputian
+{
+	using EntityID = uint64_t;
+}
---
+++ Lilliputian/Utilities/Definitions/Constants.hpp
@@ -0,0 +1,27 @@
+#pragma once
+
+namespace Lilliputian
+{
+#define MS_PER_COMPUTE_UPDATE 8
+
+#define TERA 1000000000000.0
+#define GIGA 1000000000.0
+#define MEGA 1000000.0
+#define KILO 1000.0
+#define MILLI 0.001
+#define MICRO 0.000001
+#define NANO 0.000000001
+#define PICO 0.000000000001
+
+#define NS_IN_S (1.0 / NANO)
+#define NS_IN_MS (MILLI / NANO)
+#define MS_IN_S (1.0 / MILLI)
+#define S_PER_COMPUTE_UPDATE ((double)MS_PER_COMPUTE_UPDATE / (double)MS_IN_S)
+#define COMPUTE_UPDATES_PER_S (1.0 / (double)S_PER_COMPUTE_UPDATE)
+
+#define EARTH_GRAVITY 9.80665
+#define GRAVITATIONAL_CONSTANT 6.67408e-11
+#define EARTH_MASS 5.9722e24
+
+#define NO_ENTITY 0 
+}
