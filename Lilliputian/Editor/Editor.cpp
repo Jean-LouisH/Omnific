fcfd4409ce67515046e2761442238069ebcc90bc
Jean-LouisH <LouisLou512.e@gmail.com>
2020-02-15 10:01:00 -0500

    Editor contains references to some user defined game objects and can load them

--- Lilliputian/Editor/Editor.cpp
+++ Lilliputian/Editor/Editor.cpp
@@ -2,5 +2,25 @@
 
 Lilliputian::Editor::Editor()
 {
+	this->scriptingAPI = NULL;
+}
 
+Lilliputian::Scene2D Lilliputian::Editor::createNewScene2D()
+{
+	return Scene2D();
+}
+
+Lilliputian::Script Lilliputian::Editor::createNewScript()
+{
+	return Script(this->scriptingAPI);
+}
+
+Lilliputian::Entity2D Lilliputian::Editor::createNewEntity2D()
+{
+	return Entity2D();
+}
+
+void Lilliputian::Editor::setMainScene2D(Scene2D scene2D)
+{
+	this->mainScene2D = scene2D;
 }
--- Lilliputian/Editor/Editor.hpp
+++ Lilliputian/Editor/Editor.hpp
@@ -1,12 +1,24 @@
 #pragma once
 
+#include <vector>
+#include "../Game/Scene2D.hpp"
+#include "../Game/Script.hpp"
+#include "../Game/Entity2D.hpp"
+
 namespace Lilliputian
 {
 	class Editor
 	{
 	private:
+		ScriptingAPI* scriptingAPI;
+		Scene2D mainScene2D;
+		std::vector<Scene2D> scene2Ds;
 	public:
 		Editor();
+		Scene2D createNewScene2D();
+		Script createNewScript();
+		Entity2D createNewEntity2D();
+		void setMainScene2D(Scene2D scene2D);
 	};
 }
 
