cb98d5cf0025a84a48cb9231ef9b12e1d49ba07b
Jean-LouisH <LouisLou512.e@gmail.com>
2020-06-26 13:46:29 -0400

    Simplified SDL_main undef, renamed test game title to "Lilliputian TestGame"

--- TestGame/main.cpp
+++ TestGame/main.cpp
@@ -1,15 +1,13 @@
 #include <Lilliputian.hpp>
 #include  "SceneDefiner.hpp"
 
-#ifdef main
 #undef main
-#endif
 
 int main(int argc, char* argv[])
 {
 	Lilliputian::Engine lilliputian;
 
-	lilliputian.setGameTitle("Lilliputian Demo");
+	lilliputian.setGameTitle("Lilliputian TestGame");
 	lilliputian.setWindowDimensions(480, 640);
 	lilliputian.addSceneDefiner(TestGame::splashScreen);
 	lilliputian.addSceneDefiner(TestGame::demoSelectionMenu);
