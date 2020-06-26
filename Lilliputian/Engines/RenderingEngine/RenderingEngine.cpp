eac3b3322483f20ca75495272feec0741fe393e4
Jean-LouisH <LouisLou512.e@gmail.com>
2020-06-26 13:45:42 -0400

    Added clear buffer and renderer initialization to rendering engine

--- Lilliputian/Engines/RenderingEngine/RenderingEngine.cpp
+++ Lilliputian/Engines/RenderingEngine/RenderingEngine.cpp
@@ -1,1 +1,21 @@
 #include "RenderingEngine.hpp"
+
+Lilliputian::RenderingEngine::RenderingEngine(SDL_Window* sdlWindow)
+{
+	this->sdlWindow = sdlWindow;
+	this->sdlRenderer = SDL_CreateRenderer(
+		sdlWindow,
+		-1,
+		SDL_RENDERER_ACCELERATED | SDL_RENDERER_PRESENTVSYNC);
+}
+
+void Lilliputian::RenderingEngine::clearBuffers()
+{
+	SDL_SetRenderDrawColor(this->sdlRenderer, 0, 0, 0, 255);
+	SDL_RenderClear(this->sdlRenderer);
+}
+
+void Lilliputian::RenderingEngine::render()
+{
+	SDL_RenderPresent(this->sdlRenderer);
+}
--- Lilliputian/Engines/RenderingEngine/RenderingEngine.hpp
+++ Lilliputian/Engines/RenderingEngine/RenderingEngine.hpp
@@ -1,11 +1,18 @@
 #pragma once
 
+#include <SDL.h>
+
 namespace Lilliputian
 {
 	class RenderingEngine
 	{
 	private:
+		SDL_Renderer* sdlRenderer;
+		SDL_Window* sdlWindow;
 	public:
+		RenderingEngine(SDL_Window* sdlWindow);
+		void clearBuffers();
+		void render();
 	};
 }
 
