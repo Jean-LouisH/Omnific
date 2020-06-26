9b605b8de113c1d2883271f6a4dcc57b22947919
Jean-LouisH <LouisLou512.e@gmail.com>
2020-06-26 13:44:52 -0400

    Defined keyboard, and mouse input extraction and storage in OSWindow

--- Lilliputian/OSWindow.cpp
+++ Lilliputian/OSWindow.cpp
@@ -1,7 +1,9 @@
 #include "OSWindow.hpp"
+#include <string>
 
 Lilliputian::OSWindow::OSWindow(const char* title, uint16_t width, uint16_t height, bool isFullscreen)
 {
+	this->shutdownRequest = false;
 	this->sdlWindow = SDL_CreateWindow(
 		title,
 		SDL_WINDOWPOS_CENTERED,
@@ -15,4 +17,101 @@ void Lilliputian::OSWindow::sleep(int time_ms)
 {
 	if (time_ms > 0)
 		SDL_Delay(time_ms);
+}
+
+void Lilliputian::OSWindow::detectGameControllers()
+{
+	for (int i = 0; i < this->gameControllers.size(); ++i)
+	{
+		SDL_GameControllerClose(this->gameControllers.at(i));
+		SDL_HapticClose(this->haptics.at(i));
+	}
+
+	this->gameControllers.clear();
+	this->haptics.clear();
+
+	for (int i = 0; i < SDL_NumJoysticks(); ++i)
+	{
+		if (SDL_IsGameController(i))
+		{
+			SDL_GameController* newController = SDL_GameControllerOpen(i);
+			SDL_Joystick* joystick = SDL_GameControllerGetJoystick(newController);
+			this->gameControllers.push_back(newController);
+			this->haptics.push_back(SDL_HapticOpenFromJoystick(joystick));
+			if (!this->gameControllers.back())
+				fprintf(stderr, "Could not open gamecontroller %i: %s\n", i, SDL_GetError());
+		}
+	}
+}
+
+void Lilliputian::OSWindow::pollInputEvents()
+{
+	SDL_Event SDLEvents;
+
+	this->releasedKeys.clear();
+
+	while (SDL_PollEvent(&SDLEvents))
+	{
+		switch (SDLEvents.type)
+		{
+			/*Close window button*/
+		case SDL_QUIT:
+			this->shutdownRequest = true;
+			break;
+
+			/*Keyboard inputs*/
+		case SDL_KEYDOWN:
+			KeyEvent press;
+			press.mod = SDLEvents.key.keysym.mod;
+			press.timestamp_ms = SDLEvents.key.timestamp;
+			this->pressedKeys.emplace(SDLEvents.key.keysym.sym, press);
+			break;
+		case SDL_KEYUP:
+			KeyEvent release;
+			release.mod = SDLEvents.key.keysym.mod;
+			release.timestamp_ms = SDLEvents.key.timestamp;
+			this->releasedKeys.emplace(SDLEvents.key.keysym.sym, release);
+			this->pressedKeys.erase(SDLEvents.key.keysym.sym);
+			break;
+		case SDL_MOUSEBUTTONDOWN:
+		case SDL_MOUSEBUTTONUP:
+			this->mouse.type = SDLEvents.type;
+			this->mouse.timestamp_ms = SDLEvents.button.timestamp;
+			this->mouse.windowID = SDLEvents.button.windowID;
+			this->mouse.button = SDLEvents.button.button;
+			this->mouse.buttonState = SDLEvents.button.state;
+			this->mouse.clicks = SDLEvents.button.clicks;
+			this->mouse.x = SDLEvents.button.x;
+			this->mouse.y = SDLEvents.button.y;
+			break;
+		case SDL_MOUSEMOTION:
+			this->mouse.type = SDL_MOUSEMOTION;
+			this->mouse.timestamp_ms = SDLEvents.motion.timestamp;
+			this->mouse.windowID = SDLEvents.motion.windowID;
+			this->mouse.motionState = SDLEvents.motion.state;
+			this->mouse.x = SDLEvents.motion.x;
+			this->mouse.y = SDLEvents.motion.y;
+			this->mouse.xrel = SDLEvents.motion.xrel;
+			this->mouse.yrel = SDLEvents.motion.yrel;
+			break;
+		case SDL_MOUSEWHEEL:
+			this->mouse.type = SDL_MOUSEWHEEL;
+			this->mouse.timestamp_ms = SDLEvents.wheel.timestamp;
+			this->mouse.windowID = SDLEvents.wheel.windowID;
+			this->mouse.xScroll = SDLEvents.wheel.x;
+			this->mouse.yScroll = SDLEvents.wheel.y;
+			this->mouse.direction = SDLEvents.wheel.direction;
+			break;
+		}
+	}
+}
+
+bool Lilliputian::OSWindow::hasRequestedShutdown()
+{
+	return this->shutdownRequest;
+}
+
+SDL_Window* Lilliputian::OSWindow::getSDLWindow()
+{
+	return this->sdlWindow;
 }
--- Lilliputian/OSWindow.hpp
+++ Lilliputian/OSWindow.hpp
@@ -1,6 +1,9 @@
 #pragma once
 
 #include <SDL.h>
+#include "Utilities/Collections/Map.hpp"
+#include "Utilities/Collections/Vector.hpp"
+#include "Utilities/Collections/Queue.hpp"
 
 namespace Lilliputian
 {
@@ -8,8 +11,62 @@ namespace Lilliputian
 	{
 	private:
 		SDL_Window* sdlWindow;
+
+		typedef struct KeyEvent
+		{
+			Uint16 mod;
+			Uint32 timestamp_ms;
+		}KeyEvent;
+
+		typedef struct MouseState
+		{
+			Uint32 type;
+			Uint8 clicks;
+			Sint32 x;
+			Sint32 y;
+			Sint32 xrel;
+			Sint32 yrel;
+			Sint32 xScroll;
+			Sint32 yScroll;
+			Uint32 direction;
+			Uint8 button;
+			Uint8 buttonState;
+			Uint32 motionState;
+			Uint32 timestamp_ms;
+			Uint32 windowID;
+
+			MouseState() :
+				type(0),
+				clicks(0),
+				x(0),
+				y(0),
+				xrel(0),
+				yrel(0),
+				xScroll(0),
+				yScroll(0),
+				direction(0),
+				button(0),
+				buttonState(0),
+				motionState(0),
+				timestamp_ms(0),
+				windowID(0)
+			{}
+		}MouseState;
+
+		bool shutdownRequest;
+		Map<SDL_Keycode, KeyEvent> pressedKeys;
+		Map<SDL_Keycode, KeyEvent> releasedKeys;
+		MouseState mouse;
+		Vector<SDL_GameController*> gameControllers;
+		Vector<SDL_Haptic*> haptics;
+		Vector<SDL_HapticEffect> hapticEffects;
+
 	public:
 		OSWindow(const char* title, uint16_t width, uint16_t height, bool isFullscreen);
 		void sleep(int time_ms);
+		void detectGameControllers();
+		void pollInputEvents();
+		bool hasRequestedShutdown();
+		SDL_Window* getSDLWindow();
 	};
 }
