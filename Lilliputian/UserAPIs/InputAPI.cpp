04581f68c87b5304122fa2b5e2698f1f6d78a1cc
Jean-LouisH <LouisLou512.e@gmail.com>
2020-06-27 18:52:33 -0400

    Defined all allowable inputs through the input API

--- Lilliputian/UserAPIs/InputAPI.cpp
+++ Lilliputian/UserAPIs/InputAPI.cpp
@@ -5,6 +5,21 @@ Lilliputian::InputAPI::InputAPI()
 	this->analogueStickSensitivity = 4096;
 }
 
+void Lilliputian::InputAPI::insertActionInput(const char* actionName, uint8_t inputCode)
+{
+	String actionString = actionName;
+
+	if (this->actionInputs.count(actionName))
+	{
+		this->actionInputs.at(actionName).push_back(inputCode);
+	}
+	else
+	{
+		Vector<uint8_t> inputCodes;
+		this->actionInputs.emplace(actionString, inputCodes);
+	}
+}
+
 bool Lilliputian::InputAPI::isKeyPressed(unsigned int keyCode)
 {
 	return false;
--- TestGame/QuitOnEscapeKey.cpp
+++ TestGame/QuitOnEscapeKey.cpp
@@ -2,7 +2,7 @@
 
 void TestGame::quitOnEscapeKey(Lilliputian::ScriptingAPI* API)
 {
-	if (API->input.isKeyPressed(API->input.KEY_CODES_ESCAPE))
+	if (API->input.isKeyPressed(API->input.KEY_ESCAPE))
 	{
 		API->game.quit();
 	}
