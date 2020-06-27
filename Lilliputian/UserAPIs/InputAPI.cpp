173b5b8421598c6beffb2eeec004d7b34aca5b15
Jean-LouisH <LouisLou512.e@gmail.com>
2020-06-27 17:32:09 -0400

    Defined an analogue stick sensitivity field and default value in the input API

--- Lilliputian/UserAPIs/InputAPI.cpp
+++ Lilliputian/UserAPIs/InputAPI.cpp
@@ -1,5 +1,10 @@
 #include "InputAPI.hpp"
 
+Lilliputian::InputAPI::InputAPI()
+{
+	this->analogueStickSensitivity = 4096;
+}
+
 bool Lilliputian::InputAPI::isKeyPressed(unsigned int keyCode)
 {
 	return false;
--- Lilliputian/UserAPIs/InputAPI.hpp
+++ Lilliputian/UserAPIs/InputAPI.hpp
@@ -1,5 +1,7 @@
 #pragma once
 
+#include <stdint.h>
+
 namespace Lilliputian
 {
 	class InputAPI
@@ -11,7 +13,9 @@ namespace Lilliputian
 		{
 			KEY_CODES_ESCAPE,
 		};
-		
+
+		InputAPI();
+		uint16_t analogueStickSensitivity;
 		bool isKeyPressed(unsigned int keyCode);
 	};
 }
