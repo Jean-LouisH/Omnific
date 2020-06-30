2717c73e35241113a67ac9aa25784fab149a41c0
Jean-LouisH <LouisLou512.e@gmail.com>
2020-06-30 11:25:42 -0400

    Corrected QuitOnEscapeKey to reference the input API pointer

--- TestGame/Scripts/QuitOnEscapeKey.cpp
+++ TestGame/Scripts/QuitOnEscapeKey.cpp
@@ -1,9 +1,9 @@
-#include "QuitOnEscapeKey.hpp"
+#include "Scripts.hpp"
 
 void TestGame::quitOnEscapeKey(Lilliputian::ScriptingAPI* API)
 {
-	if (API->input.isKeyPressed(API->input.KEY_ESCAPE))
+	if (API->input->isKeyPressed(API->input->KEY_ESCAPE))
 	{
-		API->game.quit();
+		API->game->quit();
 	}
 }
