b377a7c642512b67fd2e338ef8c2518d8aa2f09c
Jean-LouisH <LouisLou512.e@gmail.com>
2020-07-01 01:18:34 -0400

    Defined audio queues and playback in audio engine

--- Lilliputian/Engines/AudioEngine/AudioEngine.cpp
+++ Lilliputian/Engines/AudioEngine/AudioEngine.cpp
@@ -1,1 +1,17 @@
 #include "AudioEngine.hpp"
+#include "AudioSDL/AudioSDL.hpp"
+
+Lilliputian::AudioEngine::AudioEngine()
+{
+
+}
+
+void Lilliputian::AudioEngine::play()
+{
+	Audio::SDL::playSounds(&this->immediateSounds, &this->scheduledSounds);
+}
+
+void Lilliputian::AudioEngine::process(Scene* scene)
+{
+	//todo: build sounds lists from scene
+}
--- Lilliputian/Engines/AudioEngine/AudioEngine.hpp
+++ Lilliputian/Engines/AudioEngine/AudioEngine.hpp
@@ -1,11 +1,21 @@
 #pragma once
 
+#include <SDL_mixer.h>
+#include "../../Utilities/Collections/Queue.hpp"
+#include "../../Game/Scene.hpp"
+
 namespace Lilliputian
 {
 	class AudioEngine
 	{
 	private:
+		Queue<Mix_Chunk*> immediateSounds;
+		Queue<Mix_Chunk*> scheduledSounds;
+
+		void play();
 	public:
+		AudioEngine();
+		void process(Scene* scene);
 	};
 }
 
