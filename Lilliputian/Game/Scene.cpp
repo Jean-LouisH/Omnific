8f5967d3729be52bcc04ad7ca06441fe13075341
Jean-LouisH <LouisLou512.e@gmail.com>
2020-07-05 00:29:49 -0400

    Scenes construct new entities and add new subscenes on loading

--- Lilliputian/Game/Scene.cpp
+++ Lilliputian/Game/Scene.cpp
@@ -2,11 +2,23 @@
 
 Lilliputian::Scene::Scene()
 {
+	this->entityIDCount = 0;
+}
+
+void Lilliputian::Scene::addNewSubscene2D()
+{
 	Subscene2D subscene2D;
-	std::stack<Subscene2D> subscene2Ds;
+	Stack<Subscene2D> subscene2Ds;
 
 	subscene2Ds.push(subscene2D);
 	this->subscene2Ds.push_back(subscene2Ds);
+}
+
+Lilliputian::Entity2D Lilliputian::Scene::newEntity2D()
+{
+	Entity2D entity2D(this->entityIDCount);
+	this->entityIDCount++;
+	return entity2D;
 }
 
 void Lilliputian::Scene::addEntity2D(Entity2D entity2D)
--- Lilliputian/Game/Scene.hpp
+++ Lilliputian/Game/Scene.hpp
@@ -2,6 +2,7 @@
 
 #include "../Utilities/Collections/Vector.hpp"
 #include "../Utilities/Collections/Stack.hpp"
+#include "../Utilities/Collections/Queue.hpp"
 #include "Subscene2D.hpp"
 
 namespace Lilliputian
@@ -9,9 +10,12 @@ namespace Lilliputian
 	class Scene
 	{
 	private:
+		uint64_t entityIDCount;
 		Vector<Stack<Subscene2D>> subscene2Ds;
 	public:
 		Scene();
+		void addNewSubscene2D();
+		Entity2D newEntity2D();
 		void addEntity2D(Entity2D entity2D);
 		void executeFrameLogic();
 	};
