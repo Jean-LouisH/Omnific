58a882a7c740f33bfd7eb2038cc7f52c123d3e72
Jean-LouisH <LouisLou512.e@gmail.com>
2020-07-08 10:40:08 -0400

    Scenes keep ID counts on components, contain and load Assets

--- Lilliputian/Game/Scene.cpp
+++ Lilliputian/Game/Scene.cpp
@@ -3,9 +3,55 @@
 Lilliputian::Scene::Scene()
 {
 	this->entityIDCount = 0;
+	this->audioListenerIDCount = 0;
+	this->audioSourceIDCount = 0;
+	this->boxColliderIDCount = 0;
+	this->cameraIDCount = 0;
+	this->propertyAnimationIDCount = 0;
+	this->rigidBodyIDCount = 0;
+	this->spriteIDCount = 0;
 }
 
-void Lilliputian::Scene::addNewSubscene2D()
+Lilliputian::Audio Lilliputian::Scene::loadAudio(const char* filepath)
+{
+	if (!this->audios.count(filepath))
+	{
+		this->audios.emplace(filepath, Audio(filepath));
+		return this->audios.at(filepath);
+	}
+	else
+	{
+		return this->audios.at(filepath);
+	}
+}
+
+Lilliputian::Font Lilliputian::Scene::loadFont(const char* filepath)
+{
+	if (!this->fonts.count(filepath))
+	{
+		this->fonts.emplace(filepath, Font(filepath));
+		return this->fonts.at(filepath);
+	}
+	else
+	{
+		return this->fonts.at(filepath);
+	}
+}
+
+Lilliputian::Texture Lilliputian::Scene::loadTexture(const char* filepath)
+{
+	if (!this->textures.count(filepath))
+	{
+		this->textures.emplace(filepath, Texture(filepath));
+		return this->textures.at(filepath);
+	}
+	else
+	{
+		return this->textures.at(filepath);
+	}
+}
+
+void Lilliputian::Scene::incrementSubscene2D()
 {
 	Subscene2D subscene2D;
 	Stack<Subscene2D> subscene2Ds;
@@ -16,9 +62,43 @@ void Lilliputian::Scene::addNewSubscene2D()
 
 Lilliputian::Entity2D Lilliputian::Scene::newEntity2D()
 {
-	Entity2D entity2D(this->entityIDCount);
 	this->entityIDCount++;
-	return entity2D;
+	return Entity2D(this->entityIDCount);
+}
+
+Lilliputian::AudioListener2D Lilliputian::Scene::attachAudioListener2D(EntityID entityID)
+{
+	return AudioListener2D(entityID);
+}
+
+Lilliputian::AudioSource2D Lilliputian::Scene::attachAudioSource2D(EntityID entityID)
+{
+	return AudioSource2D(entityID);
+}
+
+Lilliputian::BoxCollider2D Lilliputian::Scene::attachBoxCollider2D(EntityID entityID)
+{
+	return BoxCollider2D(entityID);
+}
+
+Lilliputian::Camera2D Lilliputian::Scene::attachCamera2D(EntityID entityID)
+{
+	return Camera2D(entityID);
+}
+
+Lilliputian::PropertyAnimation Lilliputian::Scene::attachPropertyAnimation(EntityID entityID)
+{
+	return PropertyAnimation(entityID);
+}
+
+Lilliputian::RigidBody2D Lilliputian::Scene::attachRigidBody2D(EntityID entityID)
+{
+	return RigidBody2D(entityID);
+}
+
+Lilliputian::Sprite2D Lilliputian::Scene::attachSprite2D(EntityID entityID)
+{
+	return Sprite2D(entityID);
 }
 
 void Lilliputian::Scene::addEntity2D(Entity2D entity2D)
--- Lilliputian/Game/Scene.hpp
+++ Lilliputian/Game/Scene.hpp
@@ -3,6 +3,10 @@
 #include "../Utilities/Collections/Vector.hpp"
 #include "../Utilities/Collections/Stack.hpp"
 #include "../Utilities/Collections/Queue.hpp"
+#include "../Utilities/Collections/Map.hpp"
+#include "Audio.hpp"
+#include "Font.hpp"
+#include "Texture.hpp"
 #include "Subscene2D.hpp"
 
 namespace Lilliputian
@@ -11,11 +15,33 @@ namespace Lilliputian
 	{
 	private:
 		uint64_t entityIDCount;
+		uint64_t audioListenerIDCount;
+		uint64_t audioSourceIDCount;
+		uint64_t boxColliderIDCount;
+		uint64_t cameraIDCount;
+		uint64_t propertyAnimationIDCount;
+		uint64_t rigidBodyIDCount;
+		uint64_t spriteIDCount;
 		Vector<Stack<Subscene2D>> subscene2Ds;
+
+		Map<String, Audio> audios;
+		Map<String, Font> fonts;
+		Map<String, Texture> textures;
+
+		Audio loadAudio(const char* filepath);
+		Font loadFont(const char* filepath);
+		Texture loadTexture(const char* filepath);
 	public:
 		Scene();
-		void addNewSubscene2D();
+		void incrementSubscene2D();
 		Entity2D newEntity2D();
+		AudioListener2D attachAudioListener2D(EntityID entityID);
+		AudioSource2D attachAudioSource2D(EntityID entityID);
+		BoxCollider2D attachBoxCollider2D(EntityID entityID);
+		Camera2D attachCamera2D(EntityID entityID);
+		PropertyAnimation attachPropertyAnimation(EntityID entityID);
+		RigidBody2D attachRigidBody2D(EntityID entityID);
+		Sprite2D attachSprite2D(EntityID entityID);
 		void addEntity2D(Entity2D entity2D);
 		void executeFrameLogic();
 	};
