422fbf67f148825660a7df697ec2f35a52b4d764
Jean-LouisH <LouisLou512.e@gmail.com>
2020-09-17 17:22:49 -0400

    Defined all targeted functionality for AnimatedSprite2D.

--- Lilliputian/Game/Scene/AnimatedSprite2D.cpp
+++ Lilliputian/Game/Scene/AnimatedSprite2D.cpp
@@ -1,31 +1,155 @@
 #include "AnimatedSprite2D.hpp"
 
-void Lilliputian::AnimatedSprite2D::addTextureFrame(Texture texture)
+void Lilliputian::AnimatedSprite2D::addEmptyFrameSequence(String frameSequenceName)
 {
-	this->textureFrames.push_back(texture);
+	FrameSequence frameSequence;
+	this->frameSequences.emplace(frameSequenceName, frameSequence);
 }
 
-void Lilliputian::AnimatedSprite2D::flipHorizontally()
+void Lilliputian::AnimatedSprite2D::addFrameSequence(String frameSequenceName, FrameSequence frameSequence)
 {
-	this->flip ^= FLIP_HORIZONTAL;
+	this->frameSequences.emplace(frameSequenceName, frameSequence);
+}
+
+void Lilliputian::AnimatedSprite2D::addFrameToFrameSequence(String frameSequenceName, Frame frame)
+{
+	if (this->frameSequences.count(frameSequenceName))
+		this->frameSequences.at(frameSequenceName).push_back(frame);
+}
+
+void Lilliputian::AnimatedSprite2D::clearFrameSequences()
+{
+	this->frameSequences.clear();
+}
+
+void Lilliputian::AnimatedSprite2D::setAlpha(uint8_t value)
+{
+	if (value >= 0 && value <= 255)
+		this->alpha = value;
+}
+
+uint8_t Lilliputian::AnimatedSprite2D::getAlpha()
+{
+	return this->alpha;
+}
+
+void Lilliputian::AnimatedSprite2D::hide()
+{
+	this->alpha = 0;
+}
+
+void Lilliputian::AnimatedSprite2D::show()
+{
+	this->alpha = 255;
+}
+
+void Lilliputian::AnimatedSprite2D::setAnimationSpeed(float value_fps)
+{
+	if (value_fps > 0.0)
+		this->animationSpeed_fps = value_fps;
+}
+
+float Lilliputian::AnimatedSprite2D::getAnimationSpeed()
+{
+	return this->animationSpeed_fps;
+}
+
+void Lilliputian::AnimatedSprite2D::update(float delta_s)
+{
+	if (this->isPlaying)
+	{
+		this->frameTime_s += delta_s;
+
+		if (this->frameTime_s > (1.0 / this->animationSpeed_fps))
+		{
+			uint32_t currentFrameSequenceSize = this->getCurrentFrameSequence().size();
+
+			if (!this->isBackwards)
+				this->currentFrameIndex = 
+				this->currentFrameIndex + 1 % currentFrameSequenceSize;
+			else
+				this->currentFrameIndex = 
+				this->currentFrameIndex - 1 < 0 ? currentFrameSequenceSize - 1 : this->currentFrameIndex - 1;
+
+			this->frameTime_s = 0.0;
+		}
+	}
+}
+
+void Lilliputian::AnimatedSprite2D::play(String frameSequenceName)
+{
+	if (this->frameSequences.count(frameSequenceName))
+	{
+		this->isPlaying = true;
+		this->currentFrameSequenceName = frameSequenceName;
+	}
+}
+
+void Lilliputian::AnimatedSprite2D::play()
+{
+	this->isPlaying = true;
+}
+
+void Lilliputian::AnimatedSprite2D::pause()
+{
+	this->isPlaying = false;
+}
+
+void Lilliputian::AnimatedSprite2D::stop()
+{
+	this->pause();
+	this->currentFrameIndex = 0;
+}
+
+void Lilliputian::AnimatedSprite2D::setBackwards()
+{
+	this->isBackwards = true;
+}
+void Lilliputian::AnimatedSprite2D::setForwards()
+{
+	this->isBackwards = false;
 }
 
 void Lilliputian::AnimatedSprite2D::flipVertically()
 {
-	this->flip ^= FLIP_VERTICAL;
+	this->isFlippedVertically != this->isFlippedVertically;
 }
 
-bool Lilliputian::AnimatedSprite2D::getVerticalFlip()
+void Lilliputian::AnimatedSprite2D::flipHorizontally()
 {
-	return false;
+	this->isFlippedHorizontally != this->isFlippedHorizontally;
 }
 
-bool Lilliputian::AnimatedSprite2D::getHorizontalFlip()
+Lilliputian::AnimatedSprite2D::Frame Lilliputian::AnimatedSprite2D::getCurrentFrame()
 {
-	return false;
+	return this->getCurrentFrameSequence().at(this->currentFrameIndex);
 }
 
-Lilliputian::Vector<Lilliputian::Texture> Lilliputian::AnimatedSprite2D::getTextureFrames()
+Lilliputian::Vector<Lilliputian::String> Lilliputian::AnimatedSprite2D::getFrameSequenceNames()
 {
-	return this->textureFrames;
+	Vector<String> frameSequenceNames;
+
+	for (Map<String, FrameSequence>::iterator it = this->frameSequences.begin();
+		it != this->frameSequences.end();
+		++it)
+	{
+		frameSequenceNames.push_back(it->first);
+	}
+
+	return frameSequenceNames;
+}
+
+Lilliputian::AnimatedSprite2D::FrameSequence Lilliputian::AnimatedSprite2D::getFrameSequenceByName(String frameSequenceName)
+{
+	FrameSequence frameSequence;
+
+	if (this->frameSequences.count(frameSequenceName))
+		frameSequence = this->frameSequences.at(frameSequenceName);
+
+	return frameSequence;
+}
+
+Lilliputian::AnimatedSprite2D::FrameSequence Lilliputian::AnimatedSprite2D::getCurrentFrameSequence()
+{
+	return this->frameSequences.at(this->currentFrameSequenceName);
 }
--- Lilliputian/Game/Scene/AnimatedSprite2D.hpp
+++ Lilliputian/Game/Scene/AnimatedSprite2D.hpp
@@ -2,33 +2,61 @@
 
 #include "../../Utilities/Aliases.hpp"
 #include "../../Utilities/Collections/Vector.hpp"
+#include "../../Utilities/Collections/Map.hpp"
 #include "../../Utilities/Constants.hpp"
+#include "../../Utilities/String.hpp"
 #include "Assets/Texture.hpp"
 
 namespace Lilliputian
 {
 	class AnimatedSprite2D
 	{
+		using Frame = Texture;
+		using FrameIndex = uint16_t;
+		using FrameSequence = Vector<Frame>;
 	private:
-		enum FlipStates
-		{
-			FLIP_HORIZONTAL = 1 << 0,
-			FLIP_VERTICAL = 1 << 1
-		};
-
-		Vector<Texture> textureFrames;
-		uint8_t flip = 0;
+		Map<String, FrameSequence> frameSequences;
+		uint8_t alpha = 255;
+		float animationSpeed_fps = 12.0;
+		float frameTime_s = 0.0;
+		FrameIndex currentFrameIndex = 0;
+		bool isPlaying = false;
+		bool isBackwards = false;
+		bool isFlippedVertically = false;
+		bool isFlippedHorizontally = false;
 	public:
 		EntityID entityID = NO_ENTITY;
+		String currentFrameSequenceName = "";
 
-		uint8_t frameIndex = 0;
-		uint8_t alpha = 255;
+		void addEmptyFrameSequence(String frameSequenceName);
+		void addFrameSequence(String frameSequenceName, FrameSequence frameSequence);
+		void addFrameToFrameSequence(String frameSequenceName, Frame frame);
+		void clearFrameSequences();
 
-		void addTextureFrame(Texture texture);
-		void flipHorizontally();
+		void setAlpha(uint8_t value);
+		uint8_t getAlpha();
+		void hide();
+		void show();
+
+		void setAnimationSpeed(float value_fps);
+		float getAnimationSpeed();
+
+		void update(float delta_s);
+
+		void play(String frameSequenceName);
+		void play();
+		void pause();
+		void stop();
+
+		void setBackwards();
+		void setForwards();
+
 		void flipVertically();
-		bool getVerticalFlip();
-		bool getHorizontalFlip();
-		Vector<Texture> getTextureFrames();
+		void flipHorizontally();
+
+		Frame getCurrentFrame();
+		Vector<String> getFrameSequenceNames();
+		FrameSequence getFrameSequenceByName(String frameSequenceName);
+		FrameSequence getCurrentFrameSequence();
 	};
 }
