c83df7aa709ea0fcb86b843253dcaaf64a671a89
Jean-LouisH <LouisLou512.e@gmail.com>
2020-09-21 20:08:04 -0400

    CircleCollider2D implemented

---
+++ Lilliputian/Game/Scene/CircleCollider2D.hpp
@@ -0,0 +1,15 @@
+#pragma once
+
+#include "../../Utilities/Aliases.hpp"
+#include "../../Utilities/Constants.hpp"
+
+namespace Lilliputian
+{
+	class CircleCollider2D
+	{
+	private:
+	public:
+		EntityID entityID = NO_ENTITY;
+		float radius = 0.0;
+	};
+}
