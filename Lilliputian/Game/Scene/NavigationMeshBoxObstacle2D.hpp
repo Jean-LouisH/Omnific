9bc921165b58d277d25e3c6a27ab5a7d460d03c8
Jean-LouisH <LouisLou512.e@gmail.com>
2020-09-30 16:13:17 -0400

    Renamed NavigationMeshObstacle2D to NavigationMeshBoxObstacle2D

---
+++ Lilliputian/Game/Scene/NavigationMeshBoxObstacle2D.hpp
@@ -0,0 +1,16 @@
+#pragma once
+
+#include "../../Utilities/Aliases.hpp"
+#include "../../Utilities/Constants.hpp"
+#include "../../Utilities/AABB2D.hpp"
+
+namespace Lilliputian
+{
+	class NavigationMeshBoxObstacle2D
+	{
+	private:
+	public:
+		EntityID entityID = NO_ENTITY;
+		AABB2D aabb;
+	};
+}
