6444c003ed21830e6c5e4586c29abfb4f90d6373
Jean-LouisH <LouisLou512.e@gmail.com>
2020-11-17 10:44:42 -0500

    Added LilliputianTests unit and system testing executable

---
+++ LilliputianTests/LilliputianTests.hpp
@@ -0,0 +1,8 @@
+#include <Lilliputian.hpp>
+
+class LilliputianTests
+{
+private:
+public:
+
+};
---
+++ LilliputianTests/main.cpp
@@ -0,0 +1,7 @@
+#include "LilliputianTests.hpp"
+
+int main(int argc, char* argv[])
+{
+	LilliputianTests tests;
+	return 0;
+}
