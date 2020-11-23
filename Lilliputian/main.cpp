8f0b07c1b21abbbebfe79d36990af4b71e422525
Jean-LouisH <LouisLou512.e@gmail.com>
2020-11-23 12:05:50 -0500

    Removed the Lilliputian header given the change to standalone application design

--- Lilliputian/main.cpp
+++ Lilliputian/main.cpp
@@ -1,6 +1,10 @@
-#include "Lilliputian.hpp"
+#include "Engine.hpp"
+
+#undef main
 
 int main(int argc, char* argv[])
 {
+	Lilliputian::Engine lilliputian(argc, argv);
+	lilliputian.run();
 	return 0;
 }
--- LilliputianTests/LilliputianTests.hpp
+++ LilliputianTests/LilliputianTests.hpp
@@ -1,4 +1,4 @@
-#include <Lilliputian.hpp>
+
 
 class LilliputianTests
 {
