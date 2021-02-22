25903bb96a1b4e314d98e498407190c12a339a5a
Jean-LouisH <LouisLou512.e@gmail.com>
2021-02-22 17:47:09 -0500

    Implemented more of Camera2D

--- src/lilliputian/game/scene/components/camera_2d.cpp
+++ src/lilliputian/game/scene/components/camera_2d.cpp
@@ -1,11 +1,48 @@
 #include "camera_2d.hpp"
 
+Lilliputian::Camera2D::Camera2D()
+{
+	this->setViewportWidth(300);
+}
+
+void Lilliputian::Camera2D::resetAspect()
+{
+	this->aspect = defaultAspect;
+}
+
+Lilliputian::Rectangle Lilliputian::Camera2D::getViewportDimensions()
+{
+	return this->viewport_px;
+}
+
 void Lilliputian::Camera2D::setViewportWidth(uint32_t width_px)
 {
 	this->viewport_px.width = width_px;
+	if (keepAspect)
+		this->viewport_px.height = (uint32_t)((double)width_px / aspect);
 }
 
 void Lilliputian::Camera2D::setViewportHeight(uint32_t height_px)
 {
 	this->viewport_px.height = height_px;
+	if (keepAspect)
+		this->viewport_px.width = (uint32_t)((double)height_px * aspect);
+}
+
+void Lilliputian::Camera2D::setLimits(float left, float bottom, float top, float right)
+{
+	this->limits_px.min_px.x = left;
+	this->limits_px.min_px.y = bottom;
+	this->limits_px.max_px.x = right;
+	this->limits_px.max_px.y = top;
+}
+
+void Lilliputian::Camera2D::setKeepAspect(bool value)
+{
+	this->keepAspect = value;
+}
+
+void Lilliputian::Camera2D::setIsStreaming(bool value)
+{
+	this->isStreaming = value;
 }
--- src/lilliputian/game/scene/components/camera_2d.hpp
+++ src/lilliputian/game/scene/components/camera_2d.hpp
@@ -1,9 +1,10 @@
 #pragma once
 
-#include "../../../utilities/aliases.hpp"
-#include "../../../utilities/rectangle.hpp"
-#include "../../../utilities/vector2.hpp"
-#include "../../../utilities/constants.hpp"
+#include "utilities/aliases.hpp"
+#include "utilities/rectangle.hpp"
+#include "utilities/aabb_2d.hpp"
+#include "utilities/vector2.hpp"
+#include "utilities/constants.hpp"
 #include "component.hpp"
 
 namespace Lilliputian
@@ -11,10 +12,20 @@ namespace Lilliputian
 	class Camera2D : public Component
 	{
 	public:
-		Rectangle viewport_px;
-
+		Camera2D();
+		void resetAspect();
+		Rectangle getViewportDimensions();
 		void setViewportWidth(uint32_t width_px);
 		void setViewportHeight(uint32_t height_px);
+		void setLimits(float left, float bottom, float top, float right);
+		void setKeepAspect(bool value);
+		void setIsStreaming(bool value);
 	private:
+		const float defaultAspect = 1920.0 / 1080.0;
+		float aspect = defaultAspect;
+		AABB2D limits_px;
+		Rectangle viewport_px;
+		bool keepAspect = true;
+		bool isStreaming = true;
 	};
 }
