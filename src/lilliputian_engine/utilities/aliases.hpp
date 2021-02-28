dbed217319da199d064edd75e2398432d1b33136
Jean-LouisH <LouisLou512.e@gmail.com>
2021-02-28 12:53:37 -0500

    Updated aliases and constants for TransformID

--- src/lilliputian_engine/utilities/aliases.hpp
+++ src/lilliputian_engine/utilities/aliases.hpp
@@ -5,7 +5,8 @@
 namespace Lilliputian
 {
 	using EntityID = uint32_t;
-	using SubsceneID = uint32_t;
+	using TransformID = uint32_t;
+	using SceneTreeID = uint32_t;
 	using ComponentID = uint32_t;
 	using AssetID = uint32_t;
 	using ScriptID = uint32_t;
