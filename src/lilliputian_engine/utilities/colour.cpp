d19752348e7d2742874181c46aa2d5be67051cff
Jean-LouisH <LouisLou512.e@gmail.com>
2021-03-15 20:43:29 -0400

    Added Colour constructor for uint8_t RGBA values

--- src/lilliputian_engine/utilities/colour.cpp
+++ src/lilliputian_engine/utilities/colour.cpp
@@ -3,12 +3,20 @@
 Lilliputian::Colour::Colour()
 {
 	this->red = 1.0;
-	this->blue = 1.0;
 	this->green = 1.0;
+	this->blue = 1.0;
 	this->alpha = 1.0;
 }
 
 Lilliputian::Colour::Colour(std::string hex)
 {
 
+}
+
+Lilliputian::Colour::Colour(uint8_t red, uint8_t green, uint8_t blue, uint8_t alpha)
+{
+	this->red = float(red) / 255.0;
+	this->green = float(green) / 255.0;
+	this->blue = float(blue) / 255.0;
+	this->alpha = float(alpha) / 255.0;
 }
--- src/lilliputian_engine/utilities/colour.hpp
+++ src/lilliputian_engine/utilities/colour.hpp
@@ -10,10 +10,11 @@ namespace Lilliputian
 	public:
 		Colour();
 		Colour(std::string hex);
+		Colour(uint8_t red, uint8_t green, uint8_t blue, uint8_t alpha);
 	private:
 		float red;
-		float blue;
 		float green;
+		float blue;
 		float alpha;
 	};
 }
