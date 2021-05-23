89c7db22f46038caf2af098ddaea26eab470e8ab
Jean-LouisH <LouisLou512.e@gmail.com>
2021-05-23 00:10:20 -0400

    Script retrieves ui_text_label if it exists

--- data/editor/assets/scripts/engine_debug_time_display.py
+++ data/editor/assets/scripts/engine_debug_time_display.py
@@ -3,6 +3,8 @@ import time
 
 def on_frame():
     current_time_string = time.strftime("%H:%M:%S", time.localtime())
-    ui_text_label = lilliputian.get_scene_api().get_ui_text_label()
-    ui_text_label.set_text("Time: " + current_time_string)
+    scene_api = lilliputian.get_scene_api()
     
+    if scene_api.has_component("ui_text_label"):
+        ui_text_label = scene_api.get_ui_text_label()
+        ui_text_label.set_text("Time: " + current_time_string)
