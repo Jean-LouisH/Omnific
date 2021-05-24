81f47b7f5bf6a84538255afc4d8fa6322490c182
Jean-LouisH <LouisLou512.e@gmail.com>
2021-05-24 09:45:06 -0400

    Placed current_time_string retrieval in if block if ui_text_label is found

--- data/editor/assets/scripts/engine_debug_time_display.py
+++ data/editor/assets/scripts/engine_debug_time_display.py
@@ -2,9 +2,9 @@ import lilliputian
 import time
 
 def on_frame():
-    current_time_string = time.strftime("%H:%M:%S", time.localtime())
     scene_api = lilliputian.get_scene_api()
     
     if scene_api.has_component("ui_text_label"):
+        current_time_string = time.strftime("%H:%M:%S", time.localtime())
         ui_text_label = scene_api.get_ui_text_label()
         ui_text_label.set_text("Time: " + current_time_string)
