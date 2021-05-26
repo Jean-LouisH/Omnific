afc4780c0ec17bde8319a6c7579d1b655bf41f67
Jean-LouisH <LouisLou512.e@gmail.com>
2021-05-26 09:28:02 -0400

    Corrected scene path to call, checks for release of the escape key instead

--- data/editor/assets/scripts/debug_escape_to_splash_screen.py
+++ data/editor/assets/scripts/debug_escape_to_splash_screen.py
@@ -2,6 +2,6 @@ import lilliputian as lp
 
 
 def on_input():
-    if lp.get_input_api().is_on_press("escape"):
-        lp.get_scene_api().change_to_scene("asset/scenes/splash_screen.yml")
+    if lp.get_input_api().is_on_release("escape"):
+        lp.get_scene_api().change_to_scene("assets/scenes/splash_screen.yml")
 
