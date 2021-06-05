82c7e1445d4d6e474578f25e94c53dc7e7d10493
Jean-LouisH <LouisLou512.e@gmail.com>
2021-06-05 16:49:32 -0400

    debug_escape_to_splash_screen.py reflects the rename of "change_to_scene()" to "load_scene()" using the new SceneStorage implementation

--- data/editor/assets/scripts/debug_escape_to_splash_screen.py
+++ data/editor/assets/scripts/debug_escape_to_splash_screen.py
@@ -3,5 +3,5 @@ import lilliputian as lp
 
 def on_input():
     if lp.get_input_api().is_on_release("escape"):
-        lp.get_scene_api().change_to_scene("assets/scenes/splash_screen.yml")
+        lp.get_scene_api().load_scene("assets/scenes/splash_screen.yml")
 
