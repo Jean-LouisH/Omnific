5c0acfde12075172c4f7c9db2ab13edf4ee80c3f
Jean-LouisH <LouisLou512.e@gmail.com>
2021-06-06 15:48:25 -0400

    Simplified splash screen transition script. Countdown and scene transition is successful

--- data/editor/assets/scripts/splash_screen_transition.py
+++ data/editor/assets/scripts/splash_screen_transition.py
@@ -1,19 +1,10 @@
 import lilliputian as lp
 
-countdown_value = None
-next_scene = None
-countdown_timer = None
-
 def on_start():
-    global countdown_value, next_scene, countdown_timer
-    
     countdown_value = 5.0
-    next_scene = "assets/scenes/project_list.yml"
     countdown_timer = lp.get_scene_api().get_countdown_timer()
     countdown_timer.start(countdown_value)
 
-def on_frame():
-    global countdown_timer, next_scene
-    
-    if countdown_timer.is_finished():
-        lp.get_scene_api().load_scene(next_scene)
+def on_frame():    
+    if lp.get_scene_api().get_countdown_timer().is_finished():
+        lp.get_scene_api().load_scene("assets/scenes/debug.yml")
