9c2d9522c9bb85a0e1cea0937eda030504a7651c
Jean-LouisH <LouisLou512.e@gmail.com>
2021-06-29 20:12:18 -0400

    Set the first player number back to 0

--- data/editor/assets/scripts/debug_input_logging.py
+++ data/editor/assets/scripts/debug_input_logging.py
@@ -1,5 +1,5 @@
 import lilliputian as lp
 
 def on_input():
-    if lp.get_input_api().is_on_release(["q", "p", "button_a"], 1):
+    if lp.get_input_api().is_on_release(["q", "p", "button_a"], 0):
         lp.get_log_api().write("one of the inputs was pressed.")
