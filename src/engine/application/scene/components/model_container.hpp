fd4cd720aa4e244082886fcd231c758ed39e62bc
Jean-LouisH <LouisLou512.e@gmail.com>
2022-02-27 15:15:10 -0500

    Corrected model_container.hpp to have its include guard

--- src/engine/application/scene/components/model_container.hpp
+++ src/engine/application/scene/components/model_container.hpp
@@ -20,6 +20,8 @@
 // OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 // SOFTWARE.
 
+#pragma once
+
 #include "application/scene/assets/image.hpp"
 #include "application/scene/renderable_component.hpp"
 #include "application/scene/component.hpp"
