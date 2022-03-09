06ddf9cea8492db1a0231baccff09d8c25de38b9
Jean-LouisH <LouisLou512.e@gmail.com>
2022-03-09 09:47:17 -0500

    Enabled GL_LESS depth function

--- src/engine/systems/rendering_system/rendering_context/rendering_context.cpp
+++ src/engine/systems/rendering_system/rendering_context/rendering_context.cpp
@@ -56,6 +56,7 @@ void Omnia::RenderingContext::initialize()
 		//glEnable(GL_BLEND);
 		//glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
 		glEnable(GL_DEPTH_TEST);
+		glDepthFunc(GL_LESS);
 	}
 }
 
