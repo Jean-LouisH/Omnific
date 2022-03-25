b9e36679fbce388081cc6c2092f78606fa332250
Jean-LouisH <LouisLou512.e@gmail.com>
2022-03-25 16:17:25 -0400

    Updated event_bus demo with held input querying for smoother movement

--- data/demos/event_bus/scripts/python/move_with_input.py
+++ data/demos/event_bus/scripts/python/move_with_input.py
@@ -1,13 +1,13 @@
 import omnia
 
 def on_logic_frame():
-    movement_speed = 5.0
+    movement_speed = 1.0
     scene_api = omnia.get_scene_api()
     input_api = omnia.get_input_api()
     transform = scene_api.get_component("Transform")
 
-    if input_api.is_on_press(["s", "dpad_down"]):
+    if input_api.is_pressed(["s", "dpad_down"]):
         transform.globally_translate_y(-movement_speed)
-    elif input_api.is_on_press(["w", "dpad_up"]):
+    elif input_api.is_pressed(["w", "dpad_up"]):
         transform.globally_translate_y(movement_speed)
     
