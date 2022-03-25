3524514885b21fd464652cf602ab4529e564d023
Jean-LouisH <LouisLou512.e@gmail.com>
2022-03-25 15:01:54 -0400

    Updated event_bus demo and screenshot

--- data/demos/event_bus/scripts/python/change_texture_on_collision.py
+++ data/demos/event_bus/scripts/python/change_texture_on_collision.py
@@ -2,9 +2,10 @@ import omnia
 
 def on_start():
     scene_api = omnia.get_scene_api()
-    image = scene_api.load_asset("Image", "event_bus/images/collision.png")
+    image = scene_api.load_image("event_bus/images/collision.png")
     model_container = scene_api.get_component("ModelContainer")
     model_container.add_textured_cube(image)
+    model_container.set_model_index(0)
 
 def on_output():
     scene_api = omnia.get_scene_api()
@@ -16,9 +17,8 @@ def on_output():
 
     model_container.set_model_index(0)
 
-    #for collision in collision_events:
-    #    omnia.get_log_api().write(str(collision))
-    #    collision_strings = collision.get_parameters.strings
-    #    if (collision_strings["first_entity_name"] == "CollidingCube" and
-    #        collision_strings["second_entity_name"] == "Floor"):
-    #        model_container.set_model_index(1)
+    for collision in collision_events:
+        collision_strings = collision.get_parameters().strings
+        if (collision_strings["first_entity_name"] == "Floor" and
+            collision_strings["second_entity_name"] == "CollidingCube"):
+            model_container.set_model_index(1)
