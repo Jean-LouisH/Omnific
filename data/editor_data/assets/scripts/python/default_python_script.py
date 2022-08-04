5d26574cc6371c587b50b4ad92a16ad2008b5951
Jean-LouisH <LouisLou512.e@gmail.com>
2022-08-04 11:08:01 -0400

    Stored a string in Python script for the Editor's autogenerated default python script

---
+++ data/editor_data/assets/scripts/python/default_python_script.py
@@ -0,0 +1,15 @@
+# When generating new empty scripts, this is the
+# string to place in the file.
+
+default_python_script_string =
+
+"""import omnia
+
+class omnia_script:
+
+    def __init__(self):
+        pass
+
+    def on_logic_frame(self):
+        pass
+"""
