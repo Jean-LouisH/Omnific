915c2e602466e661afddc365ff0698d22fd92dd6
Jean-LouisH <LouisLou512.e@gmail.com>
2022-09-18 17:25:51 -0400

    Corrected  the use of translate on the camera transform

--- data/debug_data/tests/loading_models/scripts/python/camera_control.py
+++ data/debug_data/tests/loading_models/scripts/python/camera_control.py
@@ -11,9 +11,9 @@ class omnia_script:
         transform = omnia.get_component("Transform")
 
         if input.is_pressed(["s", "dpad_down"]):
-            transform.globally_translate_z(movement_speed)
+            transform.translate_z(movement_speed)
         elif input.is_pressed(["w", "dpad_up"]):
-            transform.globally_translate_z(-movement_speed)
+            transform.translate_z(-movement_speed)
     
         if input.is_on_release("p"):
             omnia.get_component("Camera").toggle_wireframe_mode()
