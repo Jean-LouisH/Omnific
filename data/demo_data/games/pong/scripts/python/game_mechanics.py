936b6d92814a5b1ca9ae7e3f4a70c892d63d471b
Jean-LouisH <LouisLou512.e@gmail.com>
2023-12-03 21:48:45 -0400

    Tweaked pong game mechanics

--- data/demo_data/games/pong/scripts/python/game_mechanics.py
+++ data/demo_data/games/pong/scripts/python/game_mechanics.py
@@ -14,7 +14,7 @@ class omnia_script:
         ball_physics_body = scene_layer.get_component("PhysicsBody", ball_entity.get_id())
         ball_transform = scene_layer.get_component("Transform", ball_entity.get_id())
         player_x_direction = random.choice([-1.0, 1.0])
-        ball_physics_body.linear_velocity.x = random.uniform(8.0, 10.0) * player_x_direction
+        ball_physics_body.linear_velocity.x = random.uniform(12.0, 14.0) * player_x_direction
         ball_physics_body.linear_velocity.y = random.uniform(5.0, 8.0)
         ball_transform.translation.x = 0.0
         ball_transform.translation.y = 1.0
@@ -55,6 +55,7 @@ class omnia_script:
         if ((collisions.is_colliding("Ball", "Paddle1") and ball_physics_body.linear_velocity.x < 0.0) or 
         (collisions.is_colliding("Ball", "Paddle2") and ball_physics_body.linear_velocity.x > 0.0)):
             ball_physics_body.linear_velocity.x *= -1.0 * ball_bounce_increase
+            ball_physics_body.linear_velocity.y = (ball_physics_body.linear_velocity.y / abs(ball_physics_body.linear_velocity.y)) * random.uniform(5.0, 16.0)
             if abs(ball_physics_body.linear_velocity.x) > constants.ball_maximum_speed:
                 if ball_physics_body.linear_velocity.x < 0.0:
                     ball_physics_body.linear_velocity.x = -constants.ball_maximum_speed
